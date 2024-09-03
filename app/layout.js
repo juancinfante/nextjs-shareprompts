39608561bcc6e9cebcd02199e07cc0f98562c6fa
Juan Cruz Infante <48840855+juancinfante@users.noreply.github.com>
2024-09-03 14:30:28 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,22 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 
+
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
-}
+    title: "Promptopia title",
+    openGraph: {
+      title: 'Title OG',
+      description: 'Description OG',
+      images: [
+        {
+          url: 'https://1.bp.blogspot.com/-AGV0I68gN6A/Wc9tkwMDBrI/AAAAAAAADgQ/UdFKfZ4b8U0flPcIcTThLD1WmsJwO0X2ACLcBGAs/s1600/hombre3.jpg', // Ruta a la imagen
+          width: 800, // Ancho de la imagen
+          height: 600, // Alto de la imagen
+          alt: 'Descripción de la imagen OG', // Descripción alternativa
+        },
+      ],
+    },
+  }
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
